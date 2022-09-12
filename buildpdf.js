99f89da37081e562b7440f0e786a048ae3dd0e3b
Jonathan Cole <jon@jons.website>
2022-09-12 16:25:44 -0400

    Remove now-unsupported in-package.json environment vars

--- buildpdf.js
+++ buildpdf.js
@@ -1,13 +1,14 @@
 const package = require("./package.json");
 const puppeteer = require("puppeteer");
 const path = require("path");
-const filePath = path.resolve(__dirname, package.resume_output_directory + "resume.html"); 
+const filePath = path.resolve(__dirname, "build/resume.html");
+console.log(`Pointing Puppeteer at ${filePath}`);
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(`file:///${filePath}`, {waitUntil: 'networkidle2'}); // Wait for network resources to load, unbreaks font-awesome
   await page.pdf({
-    path: package.resume_output_directory + "resume.pdf",
+    path: "build/resume.pdf",
     format: "Letter",
     printBackground: true,
     displayHeaderFooter: false,
